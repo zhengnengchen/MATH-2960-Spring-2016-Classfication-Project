077b24f2d2fe4f8c4aa50820b1cde0f563e519a0
Zhengneng Chen <chenz11@rpi.edu>
2016-03-28 20:28:56 -0400

    Added files via upload

---
+++ project.m
@@ -0,0 +1,28 @@
+%% Introduction to Data Mathematics Classification Project
+% Company Name: SWD Inc.
+% Nominal CEO: Takuya(Not in project group)
+% Employee: Shuang Guan, Shutong Luo, Zhengneng Chen, Ziao Yan
+
+% First, let's clean any open figures.
+clear;
+close all;
+
+% Now read raw data.
+data_initial = csvread('DatasetA.csv')
+
+%% 1. The size of data set "DatasetA.csv"
+% number of attributes
+
+% number of points in each class
+
+%% 2. mean and covariance of class 1
+% Hint: the Matlab imagesc command can be a good way to display covariances as images
+
+%% 3. mean and covariance of class -1
+% Hint: the Matlab imagesc command can be a good way to display covariances as images
+
+%% 4. normal and threshold of Mean mathod
+
+%% 5. normal and threshold of Fisher LDA
+
+%% 6.
