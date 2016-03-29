ab5332dd071f2647f55c4cad7702b55bca4a2c73
golive11035 <chenz11@rpi.edu>
2016-03-28 20:44:02 -0400

    update to 9.

--- project.m
+++ project.m
@@ -8,21 +8,41 @@ clear;
 close all;
 
 % Now read raw data.
-data_initial = csvread('DatasetA.csv')
+data_a_initial = csvread('DatasetA.csv')
 
 %% 1. The size of data set "DatasetA.csv"
 % number of attributes
 
 % number of points in each class
 
-%% 2. mean and covariance of class 1
+%% 2. Mean and covariance of class 1
 % Hint: the Matlab imagesc command can be a good way to display covariances as images
 
-%% 3. mean and covariance of class -1
+%% 3. Mean and covariance of class -1
 % Hint: the Matlab imagesc command can be a good way to display covariances as images
 
-%% 4. normal and threshold of Mean mathod
+%% 4. Normal and threshold of Mean mathod
 
-%% 5. normal and threshold of Fisher LDA
+%% 5. Normal and threshold of Fisher LDA
 
-%% 6.
+%% 6. The predictive model(Fisher LDA)
+
+% normal_pred = ; uncomment if finished
+% threshold_pred = ; uncomment if finished
+
+%% 7. Error in "DatasetA.csv"
+% Error of class 1
+
+% Error of class -1
+
+% Total error
+
+%% 8. Estimate our model's performance
+
+%% 9. Test data in "DatasetV.csv" using our model
+% Let's read data in 'DatasetV.csv' into a matrix
+data_v_initial = csvread('DatasetV.csv')
+
+% Number of points of class 1
+
+% Number of points of class -1
