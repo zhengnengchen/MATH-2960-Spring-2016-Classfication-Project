b17af8caea6f5d1015bdc49f96b4d862ed0fba6e
golive11035 <chenz11@rpi.edu>
2016-03-31 12:39:05 -0400

    Fix some bug and add report

--- project.m
+++ project.m
@@ -16,9 +16,13 @@ label = a_raw(:, (end));
 %% 1. The size of data set "DatasetA.csv"
 % Number of attributes
 attributes = size(a, 2);
+disp('Number of attributes in DatasetA is ');
+disp(attributes);
 
 % Number of points in each class
 points = size(a, 1);
+disp('Number of points in DatasetA is ');
+disp(points);
 
 %% 2. The mean and covariance of each class in 'DatasetA.csv'
 % Hint: the Matlab imagesc command can be a good way to display covariances as images
@@ -27,7 +31,10 @@ format long;
 ap = a(label == 1, :);
 ap_mean = mean(ap);
 ap_size = size(ap, 1);
-ap_cov = ap - ones(ap_size,1) * ap_mean;
+ap_var = ap - ones(ap_size,1) * ap_mean;
+ap_cov = 1/(ap_size - 1) * (ap_var' * ap_var);
+disp('Mean of class 1 in DatasetA is ');
+disp(ap_mean);
 
 % Create a grayscale heatmap of class 1's covariances
 figure
@@ -41,7 +48,10 @@ hold off
 am = a(label == -1, :);
 am_mean = mean(am);
 am_size = size(am, 1);
-am_cov = am - ones(am_size,1) * am_mean;
+am_var = am - ones(am_size,1) * am_mean;
+am_cov = 1/(am_size - 1) * (am_var' * am_var);
+disp('Mean of class -1 in DatasetA is ');
+disp(am_mean);
 
 % Create a grayscale heatmap of class -1's covariances
 figure
@@ -59,7 +69,10 @@ format long;
 
 v_mean = mean(v);
 v_size = size(v, 1);
-v_cov = v - ones(v_size,1) * v_mean;
+v_var = v - ones(v_size,1) * v_mean;
+v_cov = 1/(v_size - 1) * (v_var * v_var');
+disp('Mean of DatasetV is ');
+disp(v_mean);
 
 % Create a grayscale heatmap of class -1's covariances
 figure
@@ -102,7 +115,7 @@ am_train = a_train(label_train == -1, :);
 ap_test = a_test(label_test == 1, :);
 am_test = a_test(label_test == -1, :);
 
-%% 5. Normal and threshold of Mean mathod
+%% 5. Normal and threshold of Mean method
 format long;
 
 % Calculate w as difference of the class means
@@ -110,47 +123,63 @@ ap_train_mean = mean(ap_train);
 am_train_mean = mean(am_train);
 w_mean = (ap_train_mean - am_train_mean)';
 w_mean = w_mean / norm(w_mean);
+disp('Normal of Mean method is ');
+disp(w_mean);
 
 % Calculate threshold t
 t_mean = (ap_train_mean + am_train_mean) / 2 * w_mean;
+disp('Threshold of Mean method is ');
+disp(t_mean);
 
 %% 6. Normal and threshold of Fisher LDA
 format long;
 
-ap_train_cov = ap_train - ones(size(ap_train,1), 1) * ap_mean;
-am_train_cov = am_train - ones(size(am_train,1), 1) * am_mean;
-w_fisher = ap_train_cov' * ap_train_cov + am_train_cov' * am_train_cov;
+ap_train_var = ap_train - ones(size(ap_train,1), 1) * ap_mean;
+am_train_var = am_train - ones(size(am_train,1), 1) * am_mean;
+w_fisher = ap_train_var' * ap_train_var + am_train_var' * am_train_var;
 w_fisher = w_fisher \ (ap_mean - am_mean)';
 
 % Normal of fisher LDA
 w_fisher = w_fisher / norm(w_fisher);
+disp('Normal of Fisher LDA method is ');
+disp(w_fisher);
 
 % Threshold of Fisher LDA
 t_fisher = (ap_mean + am_mean) ./ 2 * w_fisher;
+disp('Threshold of Fisher LDA method is ');
+disp(t_fisher);
 
 %% 7. Error analysis for data of "DatasetA.csv"
 % Training error of mean method
 MeanPosErrorTrain = sum(ap_train * w_mean <= t_mean);
 MeanNegErrorTrain = sum(am_train * w_mean >= t_mean);
 MeanTrainError = (MeanPosErrorTrain + MeanNegErrorTrain) / (size(ap_train, 1) + size(am_train, 1));
+disp('Training error using Mean method is ');
+disp(MeanTrainError);
 HistClass(ap_train, am_train, w_mean, t_mean, 'Mean Method Training Results', MeanTrainError);
 
 % Training error of Fisher LDA method
 FisherPosErrorTrain = sum(ap_train * w_fisher <= t_fisher);
 FisherNegErrorTrain = sum(am_train * w_fisher >= t_fisher);
 FisherTrainError = (FisherPosErrorTrain + FisherNegErrorTrain) / (size(ap_train, 1) + size(am_train, 1));
+disp('Training error using Fisher LDA method is ');
+disp(FisherTrainError);
 HistClass(ap_train, am_train, w_fisher, t_fisher, 'Fisher LDA Method Training Results', FisherTrainError);
 
 % Testing error of mean method
 MeanPosErrorTest = sum(ap_test * w_mean <= t_mean);
 MeanNegErrorTest = sum(am_test * w_mean >= t_mean);
 MeanTestError = (MeanPosErrorTest + MeanNegErrorTest) / (size(ap_test, 1) + size(am_test, 1));
+disp('Testing error using Mean method is ');
+disp(MeanTestError);
 HistClass(ap_test, am_test, w_mean, t_mean, 'Mean Method Testing Results', MeanTestError);
 
 % Testing error of Fisher LDA method
 FisherPosErrorTest = sum(ap_test * w_fisher <= t_fisher);
 FisherNegErrorTest = sum(am_test * w_fisher >= t_fisher);
 FisherTestError = (FisherPosErrorTest + FisherNegErrorTest) / (size(ap_test, 1) + size(am_test, 1));
+disp('Testing error using Fisher LDA method is ');
+disp(FisherTestError);
 HistClass(ap_test, am_test, w_fisher, t_fisher, 'Fisher LDA Method Testing Results', FisherTestError);
 
 %% 8. The predictive model(Fisher LDA)
@@ -160,16 +189,26 @@ HistClass(ap_test, am_test, w_fisher, t_fisher, 'Fisher LDA Method Testing Resul
 % We use Fisher LDA method as our predictive model
 normal_pred = w_fisher;
 threshold_pred = t_fisher;
+disp('Normal of our predictive model is ');
+disp(normal_pred);
+disp('Threshold of our predictive model is ');
+disp(threshold_pred);
 
 %% 9. Performance of our model doing on 'DatasetA.csv'
 % Class 1 error
 ap_error = (FisherPosErrorTrain + FisherPosErrorTest) / (size(ap_train, 1) + size(ap_test, 1));
+disp('Error of class 1 using our predictive model is ')
+disp(ap_error);
 
 % Class -1 error
 am_error = (FisherNegErrorTrain + FisherNegErrorTest) / (size(am_train, 1) + size(am_test, 1));
+disp('Error of class -1 using our predictive model is ')
+disp(am_error);
 
 % Total error
 total_error = (FisherPosErrorTrain + FisherPosErrorTest + FisherNegErrorTrain + FisherNegErrorTest) / (size(a, 1));
+disp('Total error using our predictive model is ')
+disp(total_error);
 
 %% 10. Estimate our model's performance
 % As we tested a set of data in 'DatasetA.csv' by using Mean method and
@@ -191,15 +230,36 @@ v_size = size(v, 1);
 % As we already tested that class 1's error is 14.3% and class -1's error
 % is 13.9%. We can reasonable induct that class 1 and class -1's error on
 % 'DatasetV.csv' is around 14% by using Fisher LDA method.
+disp('Estimate error of class 1 and class -1 in DatasetV is around 14% and estimate total error is around 15%.')
 %% 11. Test data in "DatasetV.csv" using our model
 % Number of points of class 1
-sum(v * w_fisher > t_fisher);
+vp_size = sum(v * w_fisher > t_fisher);
+disp('Number of points of class 1 in DatasetV is ')
+disp(vp_size);
+
 % Number of points of class -1
-sum(v * w_fisher < t_fisher);
+vm_size = sum(v * w_fisher < t_fisher);
+disp('Number of points of class -1 in DatasetV is ')
+disp(vm_size);
 
 %% 12. Write into 'DatasetV.csv'
-close all;
 
 v_label = (v * w_fisher > t_fisher) - (v * w_fisher < t_fisher);
-v_raw = [v_raw, v_label];
-csvwrite('DatasetV using Fisher LDA by SWD Inc.csv', v_raw);
+v_bas = [v_raw, v_label];
+csvwrite('DatasetV using Fisher LDA by SWD Inc.csv', v_bas);
+
+%% 13. Some improvement besides our predictive model
+% As linear SVM is also a good classifier, we check if it's better than
+% Fisher LDA method
+SVMStruct = svmtrain(a_train, label_train);
+Group = svmclassify(SVMStruct, a_test);
+svm_test_error = sum(Group ~= label_test) / size(Group, 1);
+disp('Test error using Linear SVM Classifier is ');
+disp(svm_test_error);
+disp(' which is better than 16.19% of Fisher LDA method.')
+
+% Here we use Matlab build-in SVM to produce a reference label for DatasetV.
+SVMStruct = svmtrain(a, label);
+Group = svmclassify(SVMStruct, v);
+v_ref = [v_raw, Group];
+csvwrite('DatasetV using SVM by SWD Inc.csv', v_ref);
